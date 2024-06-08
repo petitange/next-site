ccd38a5d7c1dd0d655302a0be4b0da1f3b7a0122
Angel Pantoja <angelpan17@hotmail.com>
2024-06-07 22:34:22 -0600

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'SEO Title',
+ description: 'SEO Title',
+ keywords: ['SEO Title', 'SEO Title']
+};
+
+export default function AboutPage() {
+  return (
+    <>
+    <span className="text-7xl">Contact Page</span>
+    </>
+  )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'SEO Title',
+  description: 'SEO Title',
+  keywords: ['SEO Title', 'SEO Title']
+ };
+
+export default function AboutPage() {
+  return (
+    <>
+    <span className="text-7xl">Pricing Page</span>
+    </>
+  )
+}
